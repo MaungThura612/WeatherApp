151ec3d1fb8d0b16132201b4e07fd81c2ffedd11
Miguel <miibpa2@gmail.com>
2017-11-11 09:47:37 +0100

    Capítulo 12

--- app/src/main/java/com/example/miguel/weatherapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/miguel/weatherapp/ui/activities/MainActivity.kt
@@ -7,8 +7,10 @@ import android.support.v7.widget.RecyclerView
 import com.example.miguel.weatherapp.ui.adapters.ForecastListAdapter
 import com.example.miguel.weatherapp.R
 import com.example.miguel.weatherapp.domain.commands.RequestForecastCommand
+import com.example.miguel.weatherapp.domain.model.Forecast
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.longToast
+import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
@@ -22,7 +24,12 @@ class MainActivity : AppCompatActivity() {
         doAsync {
             val result = RequestForecastCommand("94943").execute()
             uiThread {
-                forecastList.adapter = ForecastListAdapter(result)
+                forecastList.adapter = ForecastListAdapter(result,object : ForecastListAdapter.OnItemClickListener{
+                    override fun invoke(forecast: Forecast) {
+                        toast(forecast.date)
+                    }
+
+                })
                 longToast("Request performed") }
         }
 
